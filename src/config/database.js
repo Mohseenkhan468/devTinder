3a2f2e2375c9d63fc9cc92d7ab2f5200e224aa6c
mohsin <mohseenkhan468@gmail.com>
2026-02-23 22:45:31 +0530

    add(url)

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 
 const connectDB=async()=>{
-    await mongoose.connect('mongodb://localhost:27017/devTinder')
+    await mongoose.connect('mongodb+srv://mohseenkhan:95j9ky1USqP7EdTJ@cluster0.7z7ktpa.mongodb.net/')
 }
 
 module.exports=connectDB;
